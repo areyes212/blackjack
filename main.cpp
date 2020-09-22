a0858e4b99d6479a2084a6ccb2ea83f8574ddf4c
Joseph <hotcup.joe@gmail.com>
2020-09-22 13:13:52 -0700

    adding main.cpp that cycles through hand until gets a blackjack

--- main.cpp
+++ main.cpp
@@ -14,53 +14,41 @@
 using namespace std;
 
 int main() {
-    Shoe s(3,2);
-    Table t;
-    s.shuffle();
-    GameMaster gm(s, t);
     
-    Hand h(s.drawCard(), s.drawCard());
+    Shoe d(3,2);
+    d.shuffle();
     
+    Hand h;
+    bool dealtBlackjack = true;
+    bool blackjack = false;
+    int i = 1;
+    while (!blackjack) {
+        if (d.getCards().size() <= 0) {
+            d.build();
+            d.shuffle();
+        }
+        
+        h.add(d.drawCard());
+        
+        if (h.busted() || h.blackjack()) {
+            cout << i << ". " << h.getTotal() << ": " 
+                << h.toString() << "\n";
+            i++;
+        }
+        
+        if (dealtBlackjack) {
+            if (h.size() == 2 && h.blackjack())
+                    blackjack = h.blackjack();
+        } 
+        else
+            blackjack = h.blackjack();
+        
+        if (h.busted())
+            h.clear();
+    }
     
-    //gm.setDealer(h);
-    
-    //cout << gm.getDealer().toString() << endl;
-    
-    
-//    Shoe d(3,2);
-//    d.shuffle();
-//    
-//    Hand h;
-//    bool dealtBlackjack = true;
-//    bool blackjack = false;
-//    int i = 1;
-//    while (!blackjack) {
-//        if (d.getCards().size() <= 0) {
-//            d.build();
-//            d.shuffle();
-//        }
-//        
-//        h.add(d.drawCard());
-//        
-//        if (h.busted() || h.blackjack()) {
-//            cout << i << ". " << h.getTotal() << ": " 
-//                << h.toString() << "\n";
-//            i++;
-//        }
-//        
-//        if (dealtBlackjack) {
-//            if (h.size() == 2 && h.blackjack())
-//                    blackjack = h.blackjack();
-//        } 
-//        else
-//            blackjack = h.blackjack();
-//        
-//        if (h.busted())
-//            h.clear();
-//    }
-//    
-//    cout << h.toString() << endl;
-//    cout << h.getTotal() << endl;
+    cout << h.toString() << endl;
+    cout << h.getTotal() << endl;
     
     return 0;
 }
